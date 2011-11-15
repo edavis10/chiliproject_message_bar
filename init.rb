27795a93ed612faa234d9316960d0b6076c6438e
Eric Davis <edavis@littlestreamsoftware.com>
2011-11-14 20:04:59 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_message_bar do
+  name 'Chiliproject Message Bar plugin'
+  author 'Author name'
+  description 'This is a plugin for ChiliProject'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
