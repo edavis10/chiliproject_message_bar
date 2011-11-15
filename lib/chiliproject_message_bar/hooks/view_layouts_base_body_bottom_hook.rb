968d72045298602bd8a8e3bad34f60c67d1f4755
Eric Davis <edavis@littlestreamsoftware.com>
2011-11-14 20:59:18 -0800

    Add a message bar

--- lib/chiliproject_message_bar/hooks/view_layouts_base_body_bottom_hook.rb
+++ lib/chiliproject_message_bar/hooks/view_layouts_base_body_bottom_hook.rb
@@ -2,7 +2,7 @@ module ChiliprojectMessageBar
   module Hooks
     class ViewLayoutsBaseBodyBottomHook < Redmine::Hook::ViewListener
       def view_layouts_base_body_bottom(context={})
-        return ''
+        context[:controller].send(:render_to_string, :partial => 'message_bars/bar')
       end
     end
   end
--- test/integration/chiliproject_message_bar/hooks/view_layouts_base_body_bottom_hook_test.rb
+++ test/integration/chiliproject_message_bar/hooks/view_layouts_base_body_bottom_hook_test.rb
@@ -5,8 +5,19 @@ class ChiliprojectMessageBar::Hooks::ViewLayoutsBaseBodyBottomTest < ActionContr
 
   context "#view_layouts_base_body_bottom" do
     setup do
+      configure_plugin
+      visit '/'
     end
 
-    should "be tested"
+    should 'show a message-bar div with content' do
+      assert has_selector?('div#message-bar')
+      assert has_content?('Test content')
+      assert has_selector?('div#message-bar a.message-bar-link')
+    end
+    
+    should 'include css to position' do
+      assert has_selector?('style#message-bar-style')
+    end
+
   end
 end
